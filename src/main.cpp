f7ceafb24ea4b40691af096f7a62fb77e1abfbb6
Oleksandr Koreniak <oleksandr.k@doubledmarketing.com>
2020-11-18 16:14:51 +0200

    build

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- */
+ */    
 
 #if  defined(_WIN32)  && defined(QT_NO_DEBUG)
 // Prevent starting console in background under windows
